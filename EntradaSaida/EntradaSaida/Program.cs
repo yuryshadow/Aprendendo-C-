e8a5f2c85854c0677cdae72d3650268a05255a0d
Yuri Carlos Rodrigues de Souza <yuryshadow@live.com>
2021-09-14 16:41:03 -0300

    Entrada e saida de dados

---
+++ EntradaSaida/EntradaSaida/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntradaSaida
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Yuri");
+            //Console.Write("Carlos");
+
+            //int codigo = Console.Read();
+            //Console.WriteLine(codigo);
+
+            string texto = Console.ReadLine();
+            Console.WriteLine(texto);
+
+            Console.ReadKey();
+        }
+    }
+}

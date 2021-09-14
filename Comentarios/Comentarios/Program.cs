6222125bebeee094f62f942329af880ad8aebfba
Yuri Carlos Rodrigues de Souza <yuryshadow@live.com>
2021-09-14 16:39:53 -0300

    Criando comentários

---
+++ Comentarios/Comentarios/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comentarios
+{
+    class Program
+    {
+        // Metodo principal
+        static void Main(string[] args)
+        {
+            #region Primeiro bloco
+            /*
+             Este é um bloco de comentario
+             Posso inserir varias linhas
+             E ainda outra linha
+            */
+            #endregion
+
+            #region Primeira parte
+            Console.WriteLine("Olá Mundo !");
+            Console.WriteLine("Olá Mundo !");
+            Console.WriteLine("Olá Mundo !");
+            Console.WriteLine("Olá Mundo !");
+            Console.WriteLine("Olá Mundo !");
+            #endregion
+
+            #region Segunda parte
+            Console.WriteLine("Olá Mundo !");
+            Console.WriteLine("Olá Mundo !");
+            Console.WriteLine("Olá Mundo !");
+            Console.WriteLine("Olá Mundo !");
+            Console.WriteLine("Olá Mundo !");
+            #endregion
+
+
+            Console.ReadKey();
+        }
+    }
+}
